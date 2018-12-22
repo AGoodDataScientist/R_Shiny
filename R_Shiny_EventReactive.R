e1cecc1fb0f4d714184981dbb4f0021afe58ee2b
Baalaaji Manivannan <agooddatascientist@gmail.com>
2018-12-22 18:04:25 +0530

    Event Reactive in Shiny R

---
+++ R_Shiny_EventReactive.R
@@ -0,0 +1,20 @@
+library(shiny)
+ui <- fluidPage(
+  headerPanel("Example for Reactive"),
+  mainPanel(
+    textInput(inputId = "textinput",label = "Enter Text Here",placeholder = "Please enter some text"),
+    actionButton(inputId = "acbutton",label = "Update"),
+    textOutput("text")
+  )
+)
+server <- function(input,output,session){
+  
+  text_Eventreacttives <- eventReactive(input$acbutton,{
+    input$textinput
+  })
+  #textoutput
+  output$text <- renderText({
+    text_Eventreacttives()
+  })
+}
+shinyApp(ui,server)
